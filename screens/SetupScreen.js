9699d3b7e977ff5cf2bb8482f83a6487b66842cd
hamida <hamida@kali>
2026-05-08 17:24:33 +0100

    version 1.0.1

--- screens/SetupScreen.js
+++ screens/SetupScreen.js
@@ -11,6 +11,58 @@ import {
 } from '../services/ble';
 import { loadAlarmTime, saveAlarmTime, loadCalibration } from '../services/storage';
 import { State } from 'react-native-ble-plx';
+import { PermissionsAndroid} from 'react-native';
+
+export async function requestBlePermissions() {
+  useEffect(() => {
+  async function init() {
+    const ok = await requestBlePermissions();
+
+    if (!ok) {
+      Alert.alert(
+        'Permissions required',
+        'Bluetooth permissions are required.'
+      );
+      return;
+    }
+
+    checkBleReady();
+  }
+
+  init();
+}, []);
+
+
+  if (Platform.OS !== 'android') return true;
+
+  try {
+    if (Platform.Version >= 31) {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ]);
+
+      return (
+        granted['android.permission.BLUETOOTH_SCAN'] === PermissionsAndroid.RESULTS.GRANTED &&
+        granted['android.permission.BLUETOOTH_CONNECT'] === PermissionsAndroid.RESULTS.GRANTED &&
+        granted['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
+      );
+    }
+
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+    );
+
+    return granted === PermissionsAndroid.RESULTS.GRANTED;
+
+  } catch (err) {
+    console.warn(err);
+    return false;
+  }
+}
+
+
 
 export default function SetupScreen({ navigation }) {
   const insets = useSafeAreaInsets();
